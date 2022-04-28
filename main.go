b2619d26f6642f20f4e1c9d7159882611b9a787a
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-04-28 17:44:39 +0200

    Continue with MAIN GO

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
-//--This is the first program about Hello World in Go...
+//--This is the main program about Connecting Servers in Go...
 package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 /* This is the main function,
 where the program start. */
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World!")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Ooops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "Hello %s\n", d)
+	})
+
+	http.HandleFunc("/novisad", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World!")
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
