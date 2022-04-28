66b6e665bc62b4880cfd07cc60606e9610a208bd
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-04-28 12:12:54 +0200

    changes commited

--- main.go
+++ main.go
@@ -6,9 +6,16 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
+		w.Write([]byte("Hello World"))
 	})
 
+	http.HandleFunc("/msg", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<msg"))
+	})
+
+	log.Println("Server starting")
 	http.ListenAndServe(":9090", nil)
+
 }
