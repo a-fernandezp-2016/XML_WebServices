0506d5502366fd1a9b9ea6c073c2fd621f0204a4
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-04-28 16:30:35 +0200

    1º program Go

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
+//--This is the first program about Hello World in Go...
 package main
 
 import (
 	"fmt"
 )
 
+/* This is the main function,
+where the program start. */
 func main() {
 	fmt.Println("Hello World!")
 }
