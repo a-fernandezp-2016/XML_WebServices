c3e572309eb98d2679484281dc24cb12500ceb7c
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-05-21 18:04:32 +0200

    next changes

--- Practices/Project1/src/app/app.module.ts
+++ Practices/Project1/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SlidebarComponent } from './slidebar/slidebar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SlidebarComponent
+    SlidebarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
